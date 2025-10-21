7983a1505b0ab2b88cc242b09b9a45ccee176242
Azeb Tesfay <azbseb@gmail.com>
2025-10-21 12:13:34 -0700

    commit

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 // Require express framework
-const express = require("express"); const PORT = 3005;
+const express = require("express"); const PORT = 3005
 // Instantiate express
-const app = express();
+const app = express()
 // Define a default route
+
+//create an Arrray for appointment 
+const submissions = []
 app.post('/', (req, res) => {
     if (fname == null || lname == null) {
-        document.getElementById("er").style.display="block";
+        document.getElementById("er").style.display = "block"
     }
     else {
-        res.sendFile(`${import.meta.dirname}/views/home2.html`);
+        res.sendFile(`${import.meta.dirname}/views/home2.html`)
     }
-});
+})
+
 
 app.listen(PORT, () => {
-console.log('Server running on http://localhost:3000');
-});
+    console.log('Server running on http://localhost:3000')
+})

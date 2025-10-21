1d86dafebc07998454339c94d1cfb7109897a647
Omnissiah <234093135+Zeta1313@users.noreply.github.com>
2025-10-21 11:59:58 -0700

    Skeleton done

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const express = require("express"); const PORT = 3005;
 // Instantiate express
 const app = express();
 // Define a default route
-app.get('/', (req, res) => {
-res.send('Hello');
-// or
-// res.sendFile(__dirname + '/home.html');
+app.post('/', (req, res) => {
+    if (fname == null || lname == null) {
+        document.getElementById("er").style.display="block";
+    }
+    else {
+        res.sendFile(`${import.meta.dirname}/views/home2.html`);
+    }
 });
-// Start the app
+
 app.listen(PORT, () => {
 console.log('Server running on http://localhost:3000');
 });

9ddc6ae7314f597cbe136fc5ba93997e53882646
Azeb Tesfay <azbseb@gmail.com>
2025-10-23 12:23:08 -0700

    Final commit

--- app.js
+++ app.js
@@ -1,49 +1,48 @@
-//import express module
 
 import express from 'express'
 
-// Require express framework
-const express = require("express")
-const PORT = 3005
-// Instantiate express
 const app = express()
-
-
+const PORT = 3005
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false }))
 
 //create an Arrray for appointment 
 const submissions = []
 
+
+
 // Define a default route
 app.get('/', (req, res) => {
     res.sendFile(`${import.meta.dirname}/views/home.html`)
+    //res.sendFile('views/home.html', { root: '.' })
+})
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`)
 })
 
 app.get('/admin', (req, res) => {
     res.send(submissions)
 })
-app.post('/first', (req, res) => {
-    if (fname == null || lname == null) {
-        document.getElementById("er").style.display = "block"
-    }
-    else {
-        const newest = {
-            fname: req.body.fname,
-            lname: req.body.lname,
-            date: req.body.date,
-            time: req.body.time,
-            timestamp: Date.now()
-        }
-        submissions.push(newest)
-        res.sendFile(`${import.meta.dirname}/views/home2.html`)
-    }
 
 
+
+// Handle form submissions
+app.post('/submissions', (req, res) => {
+
+
+    const newest = {
+        fname: req.body.fname,
+        lname: req.body.lname,
+        date: req.body.date,
+        time: req.body.time,
+        timestamp: Date.now(),
+    }
+
+    submissions.push(newest)
+    console.log(newest)
+
+    res.sendFile(`${import.meta.dirname}/views/submissions.html`)
 })
 
 
 
-
-
-app.listen(PORT, () => {
-    console.log('Server running on http://localhost:3005')
-})
---
+++ public/js/appointment.js
@@ -0,0 +1,21 @@
+document.getElementById('pizza-form').onsubmit = () => {
+
+ clearErrors()
+
+ // Flag variable to determine if form data is valid
+ let isValid = true
+
+ // Validate first name
+ let fname = document.getElementById('fname').value.trim()
+ if (!fname) {
+  document.getElementById("err-fname").style.display = "block"
+  isValid = false
+ }
+
+ // Validate last name
+ let lname = document.getElementById('lname').value.trim()
+ if (!lname) {
+  document.getElementById("err-lname").style.display = "block"
+  isValid = false
+ }
+}
